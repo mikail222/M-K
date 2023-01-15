22cee040e49d209bdf0a99652efcbc4751bacaa8
Mikail <abdulraheemmukaila6@gmail.com>
2023-01-15 09:48:20 +0100

    modification

--- src/Service/Collaboration_Company.jsx
+++ src/Service/Collaboration_Company.jsx
@@ -10,11 +10,11 @@ const Collaboration_Company = () => {
   return (
     <div data-aos="slide-up" className="w-[100%] h-[30vh]">
       <div className="flex flex-row gap-[2rem] mt-[6%] justify-center  items-center">
-        <img src={iata} alt="" className="w-[8%] h-[9vh]  mr-[1rem]" />
+        <img src={iata} alt="" className="w-[5%] h-[6vh]  mr-[1rem]" />
         <img
           src="https://pbs.twimg.com/profile_images/1324492926542598146/tQCrV1h0_400x400.jpg"
           alt=""
-          className="w-[auto] h-[9vh] rounded-[20%]"
+          className="w-[5%] h-[8vh] rounded-[20%]"
         />
         <img
           src="https://roi4cio.com/uploads/roi/company/Amadeus_Russia.jpg"
@@ -24,12 +24,12 @@ const Collaboration_Company = () => {
         <img
           src="https://tukuz.com/wp-content/uploads/2020/10/flutterwave-logo-vector.png"
           alt=""
-          className="w-[15%] h-[17vh]"
+          className="w-[13%] h-[15vh]"
         />
         <img
           src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3e/Interswitch_logo.svg/2560px-Interswitch_logo.svg.png"
           alt=""
-          className="w-[15%] h-[8vh]"
+          className="w-[10%] h-[5vh]"
         />
       </div>
     </div>
